883f62dfccb8b2dd1345cd76da6cee0aa1db7fa9
Primer <mikeshapiro92@gmail.com>
2014-07-08 16:52:10 +0300

    DataController for gps and accelerometer

--- Viciniti/src/com/svitla/viciniti/MainActivity.java
+++ Viciniti/src/com/svitla/viciniti/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.svitla.viciniti.controllers.BluetoothController;
+import com.svitla.viciniti.controllers.DataController;
 import com.svitla.viciniti.controllers.MenuController;
 import com.svitla.viciniti.controllers.PreferencesController;
 import com.svitla.viciniti.ui.fragments.LevelsListFragment;
@@ -18,122 +19,128 @@ import com.svitla.viciniti.ui.fragments.DevicesListFragment;
 
 public class MainActivity extends ActionBarActivity implements ActionBar.OnNavigationListener, OnRefreshListener {
 
-	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
-	private static int currentFragment;
-	private SwipeRefreshLayout mRefreshLayout;
+    private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+    private static int currentFragment;
+    private SwipeRefreshLayout mRefreshLayout;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_main);
 
-		PreferencesController.init(this);
+	PreferencesController.init(this);
 
-		final ActionBar actionBar = getSupportActionBar();
-//		actionBar.setDisplayShowTitleEnabled(false);
-//		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-//		actionBar.setListNavigationCallbacks(new ArrayAdapter<String>(actionBar.getThemedContext(), android.R.layout.simple_list_item_1, android.R.id.text1,
-//				new String[] { getString(R.string.title_section1), getString(R.string.title_section2), getString(R.string.title_section3), }), this);
-		showFragment(1);
-		mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.refresh);
-		mRefreshLayout.setColorScheme(android.R.color.background_light, android.R.color.black, android.R.color.white, android.R.color.black);
-		mRefreshLayout.setOnRefreshListener(this);
+	final ActionBar actionBar = getSupportActionBar();
+	// actionBar.setDisplayShowTitleEnabled(false);
+	// actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+	// actionBar.setListNavigationCallbacks(new
+	// ArrayAdapter<String>(actionBar.getThemedContext(),
+	// android.R.layout.simple_list_item_1, android.R.id.text1,
+	// new String[] { getString(R.string.title_section1),
+	// getString(R.string.title_section2),
+	// getString(R.string.title_section3), }), this);
+	showFragment(1);
+	mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.refresh);
+	mRefreshLayout.setColorScheme(android.R.color.background_light, android.R.color.black, android.R.color.white, android.R.color.black);
+	mRefreshLayout.setOnRefreshListener(this);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+	// Restore the previously serialized current dropdown position.
+	if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+	    getSupportActionBar().setSelectedNavigationItem(savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
 	}
+    }
 
-	@Override
-	public void onRestoreInstanceState(Bundle savedInstanceState) {
-		// Restore the previously serialized current dropdown position.
-		if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
-			getSupportActionBar().setSelectedNavigationItem(savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
-		}
-	}
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+	// Serialize the current dropdown position.
+	outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getSupportActionBar().getSelectedNavigationIndex());
+	super.onSaveInstanceState(outState);
+    }
 
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		// Serialize the current dropdown position.
-		outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getSupportActionBar().getSelectedNavigationIndex());
-		super.onSaveInstanceState(outState);
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+	if (currentFragment == VicinityConstants.FRAGMENT_MAIN)
+	    getMenuInflater().inflate(R.menu.main_menu, menu);
+	else if (currentFragment == VicinityConstants.FRAGMENT_LEVELS)
+	    getMenuInflater().inflate(R.menu.add_level_menu, menu);
+	return true;
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		if (currentFragment == VicinityConstants.FRAGMENT_MAIN)
-			getMenuInflater().inflate(R.menu.main_menu, menu);
-		else if (currentFragment == VicinityConstants.FRAGMENT_LEVELS)
-			getMenuInflater().inflate(R.menu.add_level_menu, menu);
-		return true;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+	int id = item.getItemId();
+	if (id == R.id.action_list_level) {
+	    showFragment(VicinityConstants.FRAGMENT_LEVELS);
+	} else if (id == R.id.action_add_level) {
+	    MenuController.addLevel(this);
+	} else if (id == R.id.action_scan_controller) {
+	    MenuController.toggleScan(this);
+	    if (BluetoothController.isScanning())
+		item.setIcon(android.R.drawable.ic_media_pause);
+	    else
+		item.setIcon(android.R.drawable.ic_media_play);
+	} else if (id == R.id.action_device_status) {
+	    MenuController.deviceStatus(this);
+	} else {
+	    showFragment(VicinityConstants.FRAGMENT_MAIN);
 	}
+	return super.onOptionsItemSelected(item);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-		if (id == R.id.action_list_level) {
-			showFragment(VicinityConstants.FRAGMENT_LEVELS);
-		} else if (id == R.id.action_add_level) {
-			MenuController.addLevel(this);
-		} else if (id == R.id.action_scan_controller) {
-			MenuController.toggleScan(this);
-			if (BluetoothController.isScanning())
-				item.setIcon(android.R.drawable.ic_media_pause);
-			else
-				item.setIcon(android.R.drawable.ic_media_play);
-		} else {
-			showFragment(VicinityConstants.FRAGMENT_MAIN);
-		}
-		return super.onOptionsItemSelected(item);
+    public void showFragment(int i) {
+	switch (i) {
+	case VicinityConstants.FRAGMENT_LEVELS:
+	    currentFragment = VicinityConstants.FRAGMENT_LEVELS;
+	    getSupportFragmentManager().beginTransaction().replace(R.id.container, LevelsListFragment.newInstance(VicinityConstants.FRAGMENT_LEVELS)).commit();
+	    supportInvalidateOptionsMenu();
+	    break;
+	case VicinityConstants.FRAGMENT_MAIN:
+	    currentFragment = VicinityConstants.FRAGMENT_MAIN;
+	    getSupportFragmentManager().beginTransaction().replace(R.id.container, DevicesListFragment.newInstance(VicinityConstants.FRAGMENT_MAIN)).commit();
+	    supportInvalidateOptionsMenu();
+	    break;
+	default:
+	    Toast.makeText(this, "Not ready yet", Toast.LENGTH_SHORT).show();
 	}
+    }
 
-	public void showFragment(int i) {
-		switch (i) {
-		case VicinityConstants.FRAGMENT_LEVELS:
-			currentFragment = VicinityConstants.FRAGMENT_LEVELS;
-			getSupportFragmentManager().beginTransaction().replace(R.id.container, LevelsListFragment.newInstance(VicinityConstants.FRAGMENT_LEVELS)).commit();
-			supportInvalidateOptionsMenu();
-			break;
-		case VicinityConstants.FRAGMENT_MAIN:
-			currentFragment = VicinityConstants.FRAGMENT_MAIN;
-			getSupportFragmentManager().beginTransaction().replace(R.id.container, DevicesListFragment.newInstance(VicinityConstants.FRAGMENT_MAIN)).commit();
-			supportInvalidateOptionsMenu();
-			break;
-		default:
-			Toast.makeText(this, "Not ready yet", Toast.LENGTH_SHORT).show();
-		}
-	}
+    @Override
+    public boolean onNavigationItemSelected(int position, long id) {
+	// When the given dropdown item is selected, show its contents in the
+	// container view.
+	showFragment(position + 1);
+	return true;
+    }
 
-	@Override
-	public boolean onNavigationItemSelected(int position, long id) {
-		// When the given dropdown item is selected, show its contents in the
-		// container view.
-		showFragment(position + 1);
-		return true;
-	}
+    @Override
+    public void onRefresh() {
+	if (!BluetoothController.isScanning())
+	    BluetoothController.scanBluetooth();
+	else
+	    Toast.makeText(this, "Already scaninng", Toast.LENGTH_SHORT).show();
+    }
 
-	@Override
-	public void onRefresh() {
-		if (!BluetoothController.isScanning())
-			BluetoothController.scanBluetooth();
-		else
-			Toast.makeText(this, "Already scaninng", Toast.LENGTH_SHORT).show();
-	}
+    public static int getCurrentFragment() {
+	return currentFragment;
+    }
 
-	public static int getCurrentFragment() {
-		return currentFragment;
-	}
+    public SwipeRefreshLayout getRefreshLayout() {
+	return mRefreshLayout;
+    }
 
-	public SwipeRefreshLayout getRefreshLayout() {
-		return mRefreshLayout;
-	}
+    @Override
+    protected void onPause() {
+	super.onPause();
+	PreferencesController.save();
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		PreferencesController.save();
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		PreferencesController.load();
-	}
+    @Override
+    protected void onResume() {
+	super.onResume();
+	PreferencesController.load();
+    }
 
 }
---
+++ Viciniti/src/com/svitla/viciniti/controllers/DataController.java
@@ -0,0 +1,78 @@
+package com.svitla.viciniti.controllers;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class DataController {
+
+    public static void requestGPSData(Context context, final TextView name, final ProgressBar progressBar) {
+	progressBar.setVisibility(View.VISIBLE);
+	LocationManager locationManager = (LocationManager) context.getSystemService(Service.LOCATION_SERVICE);
+	if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+	    Intent callGPSSettingIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+	    context.startActivity(callGPSSettingIntent);
+	}
+	final Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	if (location == null && locationManager != null) {
+	    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, new LocationListener() {
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+		}
+
+		@Override
+		public void onLocationChanged(Location location) {
+		    updateGPSViews(location, name, progressBar);
+		}
+	    });
+	} else {
+	    updateGPSViews(location, name, progressBar);
+	}
+
+    }
+
+    private static void updateGPSViews(Location location, TextView name, ProgressBar progressBar) {
+	name.setText("[Lat: " + location.getLatitude() + ", Long: " + location.getLongitude() + "]");
+	progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    public static void requestAccelerometerData(Context context, final TextView strength) {
+	SensorManager senSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+	Sensor senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	senSensorManager.registerListener(new SensorEventListener() {
+
+	    @Override
+	    public void onSensorChanged(SensorEvent sensorEvent) {
+		Sensor mySensor = sensorEvent.sensor;
+		if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+		    strength.setText("X=" + String.valueOf(sensorEvent.values[0]) + "Y=" + String.valueOf(sensorEvent.values[1]) + "Z=" + String.valueOf(sensorEvent.values[2]));
+		}
+	    }
+
+	    @Override
+	    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	    }
+	}, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+}
