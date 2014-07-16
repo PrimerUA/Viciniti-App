8bde8dfb2897e33b20451a0c4b0c55666a73ff26
PrimerUA <mikeshapiro92@gmail.com>
2014-07-16 23:37:02 +0300

    fixed plot display

--- Viciniti/src/com/svitla/viciniti/controllers/PlotController.java
+++ Viciniti/src/com/svitla/viciniti/controllers/PlotController.java
@@ -14,60 +14,63 @@ import com.svitla.viciniti.monitor.SignalMonitor;
 
 public class PlotController {
 
-    private static boolean isActive;
+	private static boolean isActive;
 
-    private static GraphView graphView;
-    private static GraphViewSeries strengthSeries;
-    private static LinearLayout contentLayout;
-    private static int columnQuantity = 0;
-    private static SignalArray localSignalArray;
-    private static int number;
+	private static GraphView graphView;
+	private static GraphViewSeries strengthSeries;
+	private static LinearLayout contentLayout;
+	private static int columnQuantity = 0;
+	private static SignalArray localSignalArray;
+	private static int number;
 
-    public static void init(Context context, LinearLayout layout, int arrayListNumber) {
-	if (SignalMonitor.getMonitorArray().size() > arrayListNumber) {
-	    localSignalArray = SignalMonitor.getMonitorArray().get(arrayListNumber);
-	    contentLayout = layout;
-	    number = arrayListNumber;
-	    graphView = new BarGraphView(context, "Bluetooth data plot for device: " + localSignalArray.getBluetoothDevice().getName());
-	    strengthSeries = new GraphViewSeries(new GraphViewData[] {});
-	    buildGraph();
-	} else {
-	    Toast.makeText(context, "No data yet", Toast.LENGTH_SHORT).show();
+	public static void init(Context context, LinearLayout layout, int arrayListNumber) {
+		if (SignalMonitor.getMonitorArray().size() > arrayListNumber) {
+			localSignalArray = SignalMonitor.getMonitorArray().get(arrayListNumber);
+			contentLayout = layout;
+			number = arrayListNumber;
+			graphView = new BarGraphView(context, "Bluetooth data plot for device: " + localSignalArray.getBluetoothDevice().getName());
+			GraphViewData data = new GraphViewData(columnQuantity++, localSignalArray.getSignalArray().get(0).getRssi());
+			strengthSeries = new GraphViewSeries(new GraphViewData[] {data});
+			buildGraph();
+		} else {
+			Toast.makeText(context, "No data yet", Toast.LENGTH_SHORT).show();
+		}
 	}
-    }
 
-    public static void updateData() {
-	if (graphView != null)
-	    contentLayout.removeView(graphView);
-	localSignalArray = SignalMonitor.getMonitorArray().get(number);
-	buildGraph();
-    }
+	public static void updateData() {
+		if (graphView != null)
+			contentLayout.removeView(graphView);
+		localSignalArray = SignalMonitor.getMonitorArray().get(number);
+		buildGraph();
+	}
 
-    private static void buildGraph() {
-	for (int i = 0; i < localSignalArray.getSignalArray().size(); i++)
-	    strengthSeries.appendData(new GraphViewData(columnQuantity++, localSignalArray.getSignalArray().get(i).getRssi()), false, VicinityConstants.PLOT_COLUMNS_NUMBER);
-	graphView.addSeries(strengthSeries);
-	contentLayout.addView(graphView);
-    }
+	private static void buildGraph() {
+		for (int i = 1; i < localSignalArray.getSignalArray().size(); i++) {
+			GraphViewData data = new GraphViewData(columnQuantity++, localSignalArray.getSignalArray().get(i).getRssi());
+			strengthSeries.appendData(data, false, VicinityConstants.PLOT_COLUMNS_NUMBER);
+		}
+		graphView.addSeries(strengthSeries);
+		contentLayout.addView(graphView);
+	}
 
-    public static GraphView getGraphView() {
-	return graphView;
-    }
+	public static GraphView getGraphView() {
+		return graphView;
+	}
 
-    public static LinearLayout getContentLayout() {
-	return contentLayout;
-    }
+	public static LinearLayout getContentLayout() {
+		return contentLayout;
+	}
 
-    public static int getColumnQuantity() {
-	return columnQuantity;
-    }
+	public static int getColumnQuantity() {
+		return columnQuantity;
+	}
 
-    public static boolean isActive() {
-	return isActive;
-    }
+	public static boolean isActive() {
+		return isActive;
+	}
 
-    public static void setActive(boolean isActive) {
-	PlotController.isActive = isActive;
-    }
+	public static void setActive(boolean isActive) {
+		PlotController.isActive = isActive;
+	}
 
 }
