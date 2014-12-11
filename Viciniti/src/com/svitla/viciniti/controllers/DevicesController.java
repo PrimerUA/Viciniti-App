f5f170d6fdacff555ae84652f31b1548c482e875
Primer <mikeshapiro92@gmail.com>
2014-12-11 19:35:22 +0200

    tablet bug fixed + mint update

--- Viciniti/src/com/svitla/viciniti/controllers/DevicesController.java
+++ Viciniti/src/com/svitla/viciniti/controllers/DevicesController.java
@@ -18,83 +18,88 @@ import com.svitla.viciniti.monitor.LevelsMonitor;
 
 public class DevicesController {
 
-	private static AlertDialog alertDialog;
+    private static AlertDialog alertDialog;
 
-	public static void checkSupervisedDevices(Context context, short rssi, BluetoothDevice bluetoothDevice) {
-		for (int i = 0; i < LevelsMonitor.getLevels().size(); i++)
-			if (rssi < LevelsMonitor.getLevels().get(i).getLevel()) {
-				securityAlert(context, LevelsMonitor.getLevels().get(i), bluetoothDevice);
-			}
+    public static void checkSupervisedDevices(Context context, short rssi, BluetoothDevice bluetoothDevice) {
+	for (int i = 0; i < LevelsMonitor.getLevels().size(); i++)
+	    if (rssi < LevelsMonitor.getLevels().get(i).getLevel()) {
+		securityAlert(context, LevelsMonitor.getLevels().get(i), bluetoothDevice);
+	    }
+    }
+
+    private static void securityAlert(final Context context, Level securityLevel, BluetoothDevice bluetoothDevice) {
+	Window w = ((ActionBarActivity) context).getWindow();
+	w.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+	w.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+	w.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+	String message = "Device with name - " + bluetoothDevice.getName() + " has breached the zone named - " + securityLevel.getName() + " (" + securityLevel.getLevel() + " dBm)";
+
+	Intent intent = new Intent();
+	intent.setAction("com.example.Broadcast");
+	intent.putExtra(VicinityConstants.ALERT_MESSAGE, message);
+	context.sendBroadcast(intent);
+
+	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+	alertDialogBuilder.setTitle("Security alert!");
+	alertDialogBuilder.setMessage(message);
+	alertDialogBuilder.setCancelable(false).setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+	    public void onClick(DialogInterface dialog, int id) {
+		dialog.dismiss();
+		NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager.cancel(0);
+	    }
+	});
+
+	try {
+	    if (alertDialog != null && alertDialog.isShowing()) {
+		alertDialog.dismiss();
+		alertDialog = null;
+	    }
+	} catch (Exception e) {
+	    // nothing
 	}
+	alertDialog = alertDialogBuilder.create();
+	alertDialog.show();
+	LogController.appendFile(message, true);
+    }
 
-	private static void securityAlert(final Context context, Level securityLevel, BluetoothDevice bluetoothDevice) {
-		Window w = ((ActionBarActivity) context).getWindow();
-		w.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-		w.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-		w.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-
-		String message = "Device with name - " + bluetoothDevice.getName() + " has breached the zone named - " + securityLevel.getName() + " ("
-				+ securityLevel.getLevel() + " dBm)";
-
-		Intent intent = new Intent();
-		intent.setAction("com.example.Broadcast");
-		intent.putExtra(VicinityConstants.ALERT_MESSAGE, message);
-		context.sendBroadcast(intent);
-
-		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-		alertDialogBuilder.setTitle("Security alert!");
-		alertDialogBuilder.setMessage(message);
-		alertDialogBuilder.setCancelable(false).setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int id) {
-				dialog.dismiss();
-				NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-				mNotificationManager.cancel(0);
-			}
-		});
-		if (alertDialog != null)
-			if (alertDialog.isShowing())
-				alertDialog.dismiss();
-		alertDialog = alertDialogBuilder.create();
-		alertDialog.show();
-		LogController.appendFile(message, true);
-	}
-
-	public static void superviseDevice(final Context context, int number) {
-		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-		final BluetoothDevice bluetoothDevice = DevicesMonitor.getDevices().get(number);
-		if (!DevicesMonitor.getSupervisedDevices().contains(bluetoothDevice)) {
-			alertDialogBuilder.setTitle("Start supervising this device?");
-			alertDialogBuilder.setMessage(bluetoothDevice.getName());
-			alertDialogBuilder.setCancelable(false).setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int id) {
-					DevicesMonitor.getSupervisedDevices().add(bluetoothDevice);
-					Toast.makeText(context, "Started supervising " + bluetoothDevice.getName(), Toast.LENGTH_SHORT).show();
-					LogController.appendFile("Started supervising " + bluetoothDevice.getName(), true);
-				}
-			}).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int id) {
-					dialog.cancel();
-				}
-			});
-			AlertDialog alertDialog = alertDialogBuilder.create();
-			alertDialog.show();
-		} else {
-			alertDialogBuilder.setTitle("Stop supervising this device?");
-			alertDialogBuilder.setMessage(bluetoothDevice.getName());
-			alertDialogBuilder.setCancelable(false).setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int id) {
-					DevicesMonitor.getSupervisedDevices().remove(bluetoothDevice);
-					Toast.makeText(context, "Stopped supervising " + bluetoothDevice.getName(), Toast.LENGTH_SHORT).show();
-					LogController.appendFile("Stopped supervising " + bluetoothDevice.getName(), true);
-				}
-			}).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int id) {
-					dialog.cancel();
-				}
-			});
-			AlertDialog alertDialog = alertDialogBuilder.create();
-			alertDialog.show();
+    public static void superviseDevice(final Context context, int number) {
+	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+	final BluetoothDevice bluetoothDevice = DevicesMonitor.getDevices().get(number);
+	if (!DevicesMonitor.getSupervisedDevices().contains(bluetoothDevice)) {
+	    alertDialogBuilder.setTitle("Start supervising this device?");
+	    alertDialogBuilder.setMessage(bluetoothDevice.getName());
+	    alertDialogBuilder.setCancelable(false).setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+		public void onClick(DialogInterface dialog, int id) {
+		    DevicesMonitor.getSupervisedDevices().add(bluetoothDevice);
+		    Toast.makeText(context, "Started supervising " + bluetoothDevice.getName(), Toast.LENGTH_SHORT).show();
+		    LogController.appendFile("Started supervising " + bluetoothDevice.getName(), true);
 		}
-
+	    }).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+		public void onClick(DialogInterface dialog, int id) {
+		    dialog.cancel();
+		}
+	    });
+	    AlertDialog alertDialog = alertDialogBuilder.create();
+	    alertDialog.show();
+	} else {
+	    alertDialogBuilder.setTitle("Stop supervising this device?");
+	    alertDialogBuilder.setMessage(bluetoothDevice.getName());
+	    alertDialogBuilder.setCancelable(false).setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+		public void onClick(DialogInterface dialog, int id) {
+		    DevicesMonitor.getSupervisedDevices().remove(bluetoothDevice);
+		    Toast.makeText(context, "Stopped supervising " + bluetoothDevice.getName(), Toast.LENGTH_SHORT).show();
+		    LogController.appendFile("Stopped supervising " + bluetoothDevice.getName(), true);
+		}
+	    }).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+		public void onClick(DialogInterface dialog, int id) {
+		    dialog.cancel();
+		}
+	    });
+	    AlertDialog alertDialog = alertDialogBuilder.create();
+	    alertDialog.show();
 	}
+
+    }
 }
--- Viciniti/src/com/svitla/viciniti/controllers/PlotController.java
+++ Viciniti/src/com/svitla/viciniti/controllers/PlotController.java
@@ -15,70 +15,77 @@ import com.svitla.viciniti.monitor.SignalMonitor;
 
 public class PlotController {
 
-	private static boolean isActive;
+    private static boolean isActive;
 
-	private static GraphView graphView;
-	private static GraphViewSeries strengthSeries;
-	private static LinearLayout contentLayout;
-	private static int columnQuantity;
-	private static SignalArray localSignalArray;
-	private static BluetoothDevice bluetoothDevice;
+    private static GraphView graphView;
+    private static GraphViewSeries strengthSeries;
+    private static LinearLayout contentLayout;
+    private static int columnQuantity;
+    private static SignalArray localSignalArray;
+    private static BluetoothDevice bluetoothDevice;
 
-	public static void init(Context context, BluetoothDevice bDevice, LinearLayout layout) {
-		isActive = true;
-		columnQuantity = 0;
-		contentLayout = layout;
-		bluetoothDevice = bDevice;
-		localSignalArray = SignalMonitor.getArrayByDevice(bDevice);
-		graphView = new BarGraphView(context, localSignalArray.getBluetoothDevice().getName());
-		graphView.getGraphViewStyle().setTextSize(context.getResources().getDimension(R.dimen.d15p));
-		GraphViewData data = new GraphViewData(columnQuantity++, Math.abs(localSignalArray.getSignalArray().get(0).getRssi()));
-		strengthSeries = new GraphViewSeries(new GraphViewData[] { data });
-		buildGraph();
+    public static void init(Context context, BluetoothDevice bDevice, LinearLayout layout) {
+	isActive = true;
+	columnQuantity = 0;
+	contentLayout = layout;
+	bluetoothDevice = bDevice;
+	localSignalArray = SignalMonitor.getArrayByDevice(bDevice);
+	if (localSignalArray != null) {
+	    graphView = new BarGraphView(context, localSignalArray.getBluetoothDevice().getName());
+	    graphView.getGraphViewStyle().setTextSize(context.getResources().getDimension(R.dimen.d15p));
+	    GraphViewData data = new GraphViewData(columnQuantity++, Math.abs(localSignalArray.getSignalArray().get(0).getRssi()));
+	    strengthSeries = new GraphViewSeries(new GraphViewData[] { data });
+	    buildGraph();
 	}
+    }
 
-	public static void updateData() {
-		if (graphView != null)
-			contentLayout.removeView(graphView);
-		localSignalArray = SignalMonitor.getArrayByDevice(bluetoothDevice);
-//		GraphViewData zeroData = new GraphViewData(0,0);
-//		GraphViewSeries levelSeries = new GraphViewSeries(new GraphViewData[] { zeroData });
-//		for (int i = 1; i < LevelsMonitor.getLevels().size(); i++) {
-//			for (int j = 1; j < VicinityConstants.PLOT_COLUMNS_NUMBER; j++) {
-//				GraphViewData data = new GraphViewData(j, LevelsMonitor.getLevels().get(i).getLevel());
-//				levelSeries.appendData(data, false, VicinityConstants.PLOT_COLUMNS_NUMBER);
-//			}
-//		}
-		buildGraph();
-	}
+    public static void updateData() {
+	if (graphView != null)
+	    contentLayout.removeView(graphView);
+	localSignalArray = SignalMonitor.getArrayByDevice(bluetoothDevice);
+	// GraphViewData zeroData = new GraphViewData(0,0);
+	// GraphViewSeries levelSeries = new GraphViewSeries(new GraphViewData[]
+	// { zeroData });
+	// for (int i = 1; i < LevelsMonitor.getLevels().size(); i++) {
+	// for (int j = 1; j < VicinityConstants.PLOT_COLUMNS_NUMBER; j++) {
+	// GraphViewData data = new GraphViewData(j,
+	// LevelsMonitor.getLevels().get(i).getLevel());
+	// levelSeries.appendData(data, false,
+	// VicinityConstants.PLOT_COLUMNS_NUMBER);
+	// }
+	// }
+	buildGraph();
+    }
 
-	private static void buildGraph() {
-		for (int i = 1; i < localSignalArray.getSignalArray().size(); i++) {
-			GraphViewData data = new GraphViewData(columnQuantity++, Math.abs(localSignalArray.getSignalArray().get(i).getRssi()));
-			strengthSeries.appendData(data, false, VicinityConstants.PLOT_COLUMNS_NUMBER);
-		}
-		graphView.addSeries(strengthSeries);
-		contentLayout.addView(graphView);
+    private static void buildGraph() {
+	if (localSignalArray != null) {
+	    for (int i = 1; i < localSignalArray.getSignalArray().size(); i++) {
+		GraphViewData data = new GraphViewData(columnQuantity++, Math.abs(localSignalArray.getSignalArray().get(i).getRssi()));
+		strengthSeries.appendData(data, false, VicinityConstants.PLOT_COLUMNS_NUMBER);
+	    }
+	    graphView.addSeries(strengthSeries);
+	    contentLayout.addView(graphView);
 	}
+    }
 
-	public static GraphView getGraphView() {
-		return graphView;
-	}
+    public static GraphView getGraphView() {
+	return graphView;
+    }
 
-	public static LinearLayout getContentLayout() {
-		return contentLayout;
-	}
+    public static LinearLayout getContentLayout() {
+	return contentLayout;
+    }
 
-	public static int getColumnQuantity() {
-		return columnQuantity;
-	}
+    public static int getColumnQuantity() {
+	return columnQuantity;
+    }
 
-	public static boolean isActive() {
-		return isActive;
-	}
+    public static boolean isActive() {
+	return isActive;
+    }
 
-	public static void setActive(boolean isActive) {
-		PlotController.isActive = isActive;
-	}
+    public static void setActive(boolean isActive) {
+	PlotController.isActive = isActive;
+    }
 
 }
